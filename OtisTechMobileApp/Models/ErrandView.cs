88cfda33b6581fa09398a8a011f2fbfc460afb20
Daniel Lenberg <danneeeee@hotmail.com>
2022-11-16 14:17:58 +0100

    model

---
+++ OtisTechMobileApp/Models/ErrandView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtisTechMobileApp.Models
+{
+    public class ErrandView
+    {
+        public int Id { get; set; }
+        public string ErrandText { get; set; }
+        public int ElevatorId { get; set; }
+        public bool Active { get; set; }
+        public string Status { get; set; }
+
+
+    }
+}
